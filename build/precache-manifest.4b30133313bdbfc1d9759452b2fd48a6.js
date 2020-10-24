3f700672e38cfe05ef144ff268151e48614d182f
黄凯欣 <44826019+huangkaixin711@users.noreply.github.com>
2020-10-24 18:40:52 +0800

    add build
    关于雪梨作业“03-路由应用”的提交

---
+++ build/precache-manifest.4b30133313bdbfc1d9759452b2fd48a6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "127b13693280be7930ba3e65c1fd8900",
+    "url": "./index.html"
+  },
+  {
+    "revision": "757f38d809f22fda40c8",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "757f38d809f22fda40c8",
+    "url": "./static/js/2.f8c39f0d.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "./static/js/2.f8c39f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e264d76afe5f10eccc4b",
+    "url": "./static/js/main.dea65331.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  },
+  {
+    "revision": "71214b2b44ab8ae1314f0f89701366bd",
+    "url": "./static/media/1.71214b2b.jpg"
+  },
+  {
+    "revision": "7cc4444029415f60730a3fe292ce3ceb",
+    "url": "./static/media/2.7cc44440.jpg"
+  },
+  {
+    "revision": "47b7ac9d6f75ad6d150b0dca55a3fbdd",
+    "url": "./static/media/3.47b7ac9d.jpg"
+  },
+  {
+    "revision": "8501c4eb52eef9bd2e78db5f1c9d9be0",
+    "url": "./static/media/4.8501c4eb.jpg"
+  },
+  {
+    "revision": "1a95720dd9fc27c28ce0380e35fd4a9c",
+    "url": "./static/media/5.1a95720d.jpg"
+  },
+  {
+    "revision": "a3ed787bd435f87a984f03580acd644e",
+    "url": "./static/media/6.a3ed787b.jpg"
+  },
+  {
+    "revision": "fb6da3f5586e6e031a175a454ccfc324",
+    "url": "./static/media/7.fb6da3f5.jpg"
+  },
+  {
+    "revision": "a4aef5f2e95d555888bcdf365382074f",
+    "url": "./static/media/bed.a4aef5f2.jpg"
+  },
+  {
+    "revision": "80bc2f595fa8a7c03d3532033b6ef73f",
+    "url": "./static/media/books.80bc2f59.jpg"
+  },
+  {
+    "revision": "838d7415205b35aca367fb25a529a309",
+    "url": "./static/media/bowl.838d7415.jpg"
+  },
+  {
+    "revision": "a9a60a810619b6d9a24924243551e890",
+    "url": "./static/media/cup.a9a60a81.jpg"
+  },
+  {
+    "revision": "4dfd834401be0a37010ce2bbddf17d38",
+    "url": "./static/media/cups.4dfd8344.jpg"
+  },
+  {
+    "revision": "52182830ceb405900765d6466fae86ef",
+    "url": "./static/media/flower.52182830.jpg"
+  },
+  {
+    "revision": "ed345e09d16a916ecde7b335de87c9d6",
+    "url": "./static/media/hat.ed345e09.jpg"
+  },
+  {
+    "revision": "613f171a9942dcbb1c53cdadfba6c3e2",
+    "url": "./static/media/shoes.613f171a.jpg"
+  },
+  {
+    "revision": "92d9e9218d1772eeb5b6fc2ab8b9ff74",
+    "url": "./static/media/sweater.92d9e921.jpg"
+  },
+  {
+    "revision": "5e627601082953540f6dccaf6273a106",
+    "url": "./static/media/teapots.5e627601.jpg"
+  }
+]);
